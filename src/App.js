f0b2eb75a8b4631e1574be38e7bf7df4e70f3428
莱维 <15720605398@163.com>
2021-06-22 23:46:59 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+//引用组件
+import Home from './views/Home';
+import About from './views/About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <HashRouter>
+        <Switch>
+          <Route exact component={Home} path="/"></Route>
+          <Route component={About} path="/about"></Route>
+        </Switch>
+      </HashRouter>
+    )
+  }
 }
 
 export default App;
