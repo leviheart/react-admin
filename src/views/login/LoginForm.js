508f51f18cd03b30199249f5843601ab643aa39c
莱维 <15720605398@163.com>
2021-06-29 00:51:05 +0800

    拦截器,接口调用,环境配置

--- src/views/login/LoginForm.js
+++ src/views/login/LoginForm.js
@@ -2,6 +2,10 @@ import React, { Component, Fragment } from "react";
 //ANTD
 import { Form, Input, Button, Row, Col } from 'antd';
 import { UserOutlined, UnlockOutlined } from '@ant-design/icons';
+//验证
+import { validate_password } from "../../utils/validate"
+//API
+import { Login } from "../../api/account";
 
 class LoginForm extends Component {
     constructor() {
@@ -10,6 +14,11 @@ class LoginForm extends Component {
     }
 
     onFinish = (values) => {
+        Login().then(response => {
+            console.log("成功调用接口")
+        }).catch(error => {
+
+        })
         console.log('Received values of form: ', values);
     }
 
@@ -28,17 +37,49 @@ class LoginForm extends Component {
                 </div>
                 {/* 输入框 */}
                 <div className="form-content">
-                    <Form name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={() => this.onFinish}>
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        initialValues={{ remember: true }}
+                        onFinish={this.onFinish}>
                         {/* 用户名输入框 */}
-                        <Form.Item name="username" rules={[{ required: true, message: 'Please input your Username!' }]}>
-                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                        <Form.Item name="username" rules={
+                            [
+                                { required: true, message: '邮箱不能为空' },
+                                { type: "email", message: "邮箱格式不正确" }
+                            ]
+                        }>
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="email" />
                         </Form.Item>
                         {/* 密码输入框 */}
-                        <Form.Item name="password" rules={[{ required: true, message: 'Please input your Password!' }]}>
+                        <Form.Item name="password" rules={
+                            [
+                                { required: true, message: '密码不能为空' },
+                                // ({ getFieldValue }) => ({
+                                //     validator(rule, value) {
+                                //         if (value.length < 6) {
+                                //             return Promise.reject('不能小于6位');
+                                //         }
+                                //         else {
+                                //             return Promise.resolve();
+                                //         }
+                                //     },
+                                // }),
+                                // { min: 6, message: "不能小于6位" },
+                                // { max: 20, message: "不能大于20位" },
+                                { pattern: validate_password, message: "请输入大于6位小于20位数字+字母" },
+                            ]
+                        }>
                             <Input prefix={<UnlockOutlined className="site-form-item-icon" />} placeholder="Password" />
                         </Form.Item>
                         {/* 验证码输入框 分列*/}
-                        <Form.Item>
+                        <Form.Item name="code" rules={
+                            [
+                                { required: true, message: '验证码不能为空' },
+                                { len: 6, message: "请输入长度为6位的验证码" }
+                            ]
+                        }
+                        >
                             <Row gutter={13}>
                                 <Col span={15}>
                                     <Input prefix={<UnlockOutlined className="site-form-item-icon" />} placeholder="Code" />
@@ -54,7 +95,7 @@ class LoginForm extends Component {
                         </Form.Item>
                     </Form>
                 </div>
-            </Fragment>
+            </Fragment >
         )
     }
 }
