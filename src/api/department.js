41eefc388195b3261ba828fdf76cfa7b7c377dab
莱维 <15720605398@163.com>
2021-08-17 22:09:32 +0800

    列表接口联调、删除接口联调、引用路径配置、table组件

--- src/api/department.js
+++ src/api/department.js
@@ -2,7 +2,7 @@ import { func } from "prop-types";
 import service from "../utils/request";
 
 /**
- * 登录接口
+ * 添加
  */
 export function DepartmentAddApi(data) {
     return service.request({
@@ -12,3 +12,24 @@ export function DepartmentAddApi(data) {
     })
 }
 
+/**
+ * 获取列表
+ */
+export function GetList(data) {
+    return service.request({
+        url: "/department/list/",
+        method: "post",
+        data,
+    })
+}
+
+/**
+ * 删除
+ */
+export function Delete(data) {
+    return service.request({
+        url: "/department/delete/",
+        method: "post",
+        data,
+    })
+}
